14db02ba1f395de535c4bd57d8bcb03ee9e16f05
wangshuai <wangshuai@pset.suntec.net>
2016-01-27 11:39:22 +0800

    add shell Copy.sh
    Change-Id: Ibf2b7d721db60ccedb86e4884ed9c0d0d9fb8559

--- Shell/CopyTo.sh
+++ Shell/CopyTo.sh
@@ -5,8 +5,10 @@
 
 roo_path=${ANDROID_BUILD_TOP}
 root_out=${ANDROID_PRODUCT_OUT}${jupiter_Model}
+#root_out=${ANDROID_PRODUCT_OUT}
 root_system_lib=${root_out}/system/lib
 root_system_bin=${root_out}/system/bin
+root_system_app=${root_out}/system/app
 
 current_code=`pwd`
 current_path=${root_code}
@@ -14,65 +16,28 @@ current_path=${root_code}
 if [ $# -ne 1 ]; then
 	echo -e "\033[31m""Parameter number error, num="$#"\033[0m"
 	echo "END..."
-	return 1
+	exit
 fi
 
 if [ ! -d $1 ]; then
 	echo $1 : not a entry
 	echo "END..."
-	return 1
+	exit
 fi
 
-LibFile=( "libNPuComHandler.so"
-          "libNPSysCtrlHandler.so"
-          "libNPKeyHandler.so"
-          "libtouchcalib_if.so"
-          "libdebugmodeproxy_jni.so"
-          "libsysdIF.so"
-          "libPFileService.so"
-          "libEmuSyscon.so"
-          "libPSystemService.so"
-          "libNPHandlerBase.so"
-          "libNPCanBusHandler.so"
-          "libNPEtcHandler.so"
-          "libNPDsrcHandler.so"
-          "libNPEtcDsrcDivHandler.so"
-          "libNPHdArtHandler.so"
-          "libNPiDatalinkHandler.so"
-          "libNPPnpHandler.so"
-          "libNPRdsTmcHandler.so"
-          "libPCommonLib.so"
-          "libPSocketLib.so"
-          "libEmuSender.so"
-          "libsysconDab.so"
-          "libsysconSetting.so"
-          "libSysconTag.so"
-          "libsysconTuner.so"
-          "libsysconHDRadio.so"
-          "libsyscondivx.so"
-          "libsysconDvd.so"
-          "libechocancel.so"
-          "libNPPortLib.so"
-          "libOomAdj.so"
-          "libNPVicsFMHandler.so"
-          "libNPVicsFMData.so"
+LibFile=( "libTestMode.so"
+          "libTestModeProxy.so"
+          "libucomTestMode.so"
         )
 
-BinFile=( "sysd"
-          "ctst_sysdIFLib"
-          "test_sysdIF"
-          "IamSyscon"
-          "emusyscon"
-          "PFileSDOperator"
-          "PFileUSB1Operator"
-          "PFileUSB2Operator"
+BinFile=( "TestModeService"
+          "TestModeR"
         )
-        
 
+AppFile=( "TestMode.apk"
+        )
 
-
-
-echo "ooxx" | sudo -S  echo "Start..."
+echo "\ " | sudo -S  echo "Start..."
 
 for name in ${LibFile[@]}
 do
@@ -86,11 +51,12 @@ do
   ls -l $1/bin/${name}
 done
 
-
+for name in ${AppFile[@]}
+do
+  sudo cp ${root_system_app}/${name}  $1/app/
+  ls -l $1/app/${name}
+done
 
 sync
 echo "END..."
 echo
-
-
-
